ca1b47acfcfc0007888b4bbd90807fefb88affb1
Linh Nguyen <libh1004@gmail.com>
2021-03-05 20:03:33 +0700

    nop assignment 1

---
+++ src/asm/s1/Main.java
@@ -0,0 +1,20 @@
+//package asm.s1;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        // nhap 3 so tu ban phim
+//        Scanner sc = new Scanner(System.in);
+//        double a = sc.nextInt();
+//        double b = sc.nextInt();
+//        double c = sc.nextInt();
+//
+//        TamGiac tg = new TamGiac();
+//        tg.setCanh1(a);
+//        tg.setCanh2(b);
+//        tg.setCanh3(c);
+//        System.out.println("Chu vi: "+tg.chuVi());
+//        System.out.println("Dien tich: "+tg.dienTich();
+//    }
+//}
---
+++ src/asm/s1/TamGiac.java
@@ -0,0 +1,32 @@
+//package asm.s1;
+//
+//public class TamGiac {
+//    double Canh1;
+//    double Canh2;
+//    double Canh3;
+//
+//    public TamGiac(double canh1) {
+//        this.Canh1 = Canh1;
+//    }
+//
+//    public double getCanh1() {
+//        return Canh1;
+//    }
+//
+//    public void setCanh1(double canh1) {
+//        this.canh1 = canh1;
+//    }
+//
+//    public boolean kiemTra(double a, double b, double c){
+//        if(canh1 > 0 && canh2>0 && canh3>0 && canh1+canh2>0 && canh2+canh3>0 && canh1+canh3 > 0)return true;
+//        return false;
+//    }
+//
+//    public double chuVi(){
+//        return canh1+canh2+canh3;
+//    }
+//    public void dienTich(){
+//        double p =
+//
+//    }
+//}
